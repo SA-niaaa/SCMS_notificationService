5c4b98078a08a96c4253c55311fbd32764649030
Sania <reangsania8516@gmail.com>
2026-06-23 11:58:09 +0530

    Add notification service
    Add notification service

---
+++ notificationService/src/main/java/com/demo/notification/NotificationApplication.java
@@ -0,0 +1,15 @@
+package com.demo.notification;
+
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableRabbit
+public class NotificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationApplication.class, args);
+	}
+
+}
